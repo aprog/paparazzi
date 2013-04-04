1867ffd757c6a9bf1c42e50153dfedbbbf1f54e1
Aleksandr Kyrychok <aleksandr.kyrychok@acropolium.com>
2013-04-04 19:47:16 +0300

    Improved stub-generation script: ability to select random files with random quantity

--- generate_places.sh
+++ generate_places.sh
@@ -3,7 +3,7 @@
 PLACES_COUNT=100
 MAX_FILES_PER_RECORD=10
 
-for (( i=0; i<$PLACES_COUNT; i++))
+for (( i=0; i<$PLACES_COUNT; i++ ))
 do
     random_uid=$(curl -s -X GET localhost:3000/users | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
     random_celeb=$(curl -s -X GET localhost:3000/celebs | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
@@ -21,21 +21,35 @@ do
 	long=-$long
     fi
 
-    file=$(ls | sort -R | head -n 1)
+    files=""
+    files_random_count=$(shuf -i 0-$MAX_FILES_PER_RECORD -n 1)
+
+    for (( j=0; j<$files_random_count; j++ ))
+    do
+	if [ -z "$files" ]; then
+	    files="-F photos=@"$(ls -pA | grep -v "/$" | sort -R | head -n 1)
+	else
+	    files=$files" -F photos=@"$(ls -pA | grep -v "/$" | sort -R | head -n 1)
+	fi
+    done
+
+#    echo -e "$files"
+#    echo -e "\n\n\n\n\n"
+#    continue
 
     curl -X POST \
-       -F userId=$random_uid \
-       -F celebId=$random_celeb \
-       -F message="$message" \
-       -F loc[lat]=$lat \
-       -F loc[long]=$long \
-       -F photos=@/home/alex/.bashrc \
-       -F photos=@/home/alex/.bash_profile \
-       localhost:3000/place
+	-F userId=$random_uid \
+	-F celebId=$random_celeb \
+	-F message="$message" \
+	-F loc[lat]=$lat \
+	-F loc[long]=$long \
+	$files \
+        localhost:3000/place
+
     if [ $? -eq 0 ]; then
-        echo "Added place with location:" $lat $long
+        echo -e "\nAdded place with location:" $lat $long
     else
-	echo "Can not run curl command"
+	echo "\nCan not run curl command"
     fi
 
 done
