0cf91f5c5ddd5b44376e5cf9a7da79abca5f7d0e
Aleksandr Kyrychok <aleksandr.kyrychok@acropolium.com>
2013-04-04 16:26:31 +0300

    Improved stub-generate script: modified location generation

--- generate_places.sh
+++ generate_places.sh
@@ -5,23 +5,23 @@ MAX_FILES_PER_RECORD=10
 
 for (( i=0; i<$PLACES_COUNT; i++))
 do
-    file=$(ls | sort -R | head -n 1)
-
     random_uid=$(curl -s -X GET localhost:3000/users | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
     random_celeb=$(curl -s -X GET localhost:3000/celebs | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
     message="Etiam ultricies nisi vel augue."
 
-    lat=$(shuf -i 0-90 -n 1)
+    lat=$(shuf -i 0-90 -n 1).$(shuf -i 0-10000 -n 1)
     lat_sign=$(shuf -i 0-1 -n 1)
     if [ $lat_sign != 0 ]; then
 	lat=-$lat
     fi
 
-    long=$(shuf -i 0-180 -n 1)
+    long=$(shuf -i 0-180 -n 1).$(shuf -i 0-10000 -n 1)
     long_sign=$(shuf -i 0-1 -n 1)
     if [ $long_sign != 0 ]; then
 	long=-$long
     fi
+
+    file=$(ls | sort -R | head -n 1)
 
     curl -X POST \
        -F userId=$random_uid \
