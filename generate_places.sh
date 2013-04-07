499887a55fd1b62e016d6eb1d02c2b1c579827e2
alex <alex@localhost.localdomain>
2013-04-07 18:11:12 +0300

    Modified Place generation script with new address schema

--- generate_places.sh
+++ generate_places.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 PLACES_COUNT=500
-MAX_FILES_PER_RECORD=5
+MAX_FILES_PER_RECORD=3
 
 for (( i=0; i<$PLACES_COUNT; i++ ))
 do
-    random_uid=$(curl -s -X GET localhost:3000/users | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
-    random_celeb=$(curl -s -X GET localhost:3000/celebs | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
+    random_uid=$(curl -s -X GET localhost:3000/user/list | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
+    random_celeb=$(curl -s -X GET localhost:3000/celeb/list | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
     message="Etiam ultricies nisi vel augue."
 
     lat=$(shuf -i 0-90 -n 1).$(shuf -i 0-10000 -n 1)
