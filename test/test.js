53147f4b4a44c675e6ff39e5748bd62d22606e6b
alex <alex@localhost.localdomain>
2013-05-02 16:59:30 +0300

    Added tests for Celebrity model

--- test/test.js
+++ test/test.js
@@ -5,6 +5,8 @@ var assert = require('assert');
 var request = require('request');
 require('should');
 
+var privilegedUser = null;
+
 describe('Array', function() {
     describe('#indexOf()', function() {
         it('should return -1 when the value is not present', function() {
@@ -17,7 +19,19 @@ describe('Array', function() {
     });
 });
 
-describe('User', function() {
+describe('Privileged user', function() {
+    it('should retrieve user with admin privileges', function(done) {
+        request('http://localhost:3000/user/00000000000000000000aaee', function(error, response, body) {
+            response.statusCode.should.equal(200);
+            body.should.not.be.empty;
+            privilegedUser = JSON.parse(body);
+            privilegedUser.roles.should.include('admin');
+            done();
+        });
+    });
+});
+
+describe.skip('User', function() {
     describe('#list()', function() {
         it('should retrieve list of users without error', function(done) {
             request('http://localhost:3000/user/list', function(error, response, body) {
@@ -44,17 +58,8 @@ describe('User', function() {
         });
     });
 
-    var privilegedUser = null;
     var newUserResponse = null;
     describe('#create()', function() {
-        it('should retrieve user with admin privileges', function(done) {
-            request('http://localhost:3000/user/00000000000000000000aaee', function(error, response, body) {
-                response.statusCode.should.equal(200);
-                privilegedUser = JSON.parse(body);
-                privilegedUser.roles.should.include('admin');
-                done();
-            });
-        });
 
         it('should create a user without error', function(done) {
             request.post('http://localhost:3000/user', {
@@ -130,4 +135,121 @@ describe('User', function() {
             });
         });
     });
+});
+
+describe('Celebrity', function() {
+    describe('#list()', function() {
+        it('should retrieve list of celebrities', function(done) {
+            request('http://localhost:3000/celeb/list', function(e, r, body) {
+                r.statusCode.should.equal(200);
+                var parsedResponse = JSON.parse(body);
+                parsedResponse.should.be.an.instanceOf(Array);
+
+                var randomPosition = Math.floor(Math.random() * parsedResponse.length);
+                var randomCelebrity = parsedResponse[randomPosition];
+                randomCelebrity.should.have.property('name');
+                randomCelebrity.name.should.not.be.empty;
+
+                randomCelebrity.should.have.property('about');
+                done();
+            });
+        });
+    });
+
+    var newCelebResponse = null;
+    describe('#create()', function() {
+        it('should create celebrity', function(done) {
+            request.post('http://localhost:3000/celeb', {
+                form: {
+                    name: 'testCelebrityName',
+                    about: 'Some text, that usually places in about field',
+                    authToken: privilegedUser.token
+                }
+            }, function(e, r, body) {
+                r.statusCode.should.equal(200);
+                body.should.not.be.empty;
+                newCelebResponse = JSON.parse(body);
+                newCelebResponse.should.have.property('celebId');
+                done();
+            });
+        });
+        it('should retrieve newly created celebrity', function(done) {
+            request('http://localhost:3000/celeb/' + newCelebResponse.celebId, function(e, r, body) {
+                r.statusCode.should.equal(200);
+                body.should.not.be.empty;
+                var newCeleb = JSON.parse(body);
+                newCeleb.should.have.property('name');
+                newCeleb.name.should.equal('testCelebrityName');
+                newCeleb.should.have.property('about');
+                newCeleb.about.should.equal('Some text, that usually places in about field');
+                done();
+            });
+        });
+    });
+    describe('#remove()', function() {
+        it('should remove celebrity with specified id', function(done) {
+            request.del('http://localhost:3000/celeb/' + newCelebResponse.celebId, {
+                form: {
+                    authToken: privilegedUser.token
+                }
+            }, function(e, r) {
+                r.statusCode.should.equal(200);
+                done();
+            });
+        });
+        it('should not retrieve deleted celebrity', function(done) {
+            request('http://localhost:3000/user/' + newCelebResponse.celebId, function(e, r) {
+                r.statusCode.should.equal(404);
+                done();
+            });
+        });
+    });
+
+    describe('#create() with empty about field', function() {
+        it('should create celebrity with empty about field', function(done) {
+            request.post('http://localhost:3000/celeb', {
+                form: {
+                    name: 'testCelebrityName',
+                    authToken: privilegedUser.token
+                }
+            }, function(e, r, body) {
+                r.statusCode.should.equal(200);
+                body.should.not.be.empty;
+                newCelebResponse = JSON.parse(body);
+                newCelebResponse.should.have.property('celebId');
+                done();
+            });
+        });
+        it('should retrieve newly created celebrity with empty about field', function(done) {
+            request('http://localhost:3000/celeb/' + newCelebResponse.celebId, function(e, r, body) {
+                r.statusCode.should.equal(200);
+                body.should.not.be.empty;
+                var newCeleb = JSON.parse(body);
+                newCeleb.should.have.property('name');
+                newCeleb.name.should.equal('testCelebrityName');
+                newCeleb.should.have.property('about');
+                newCeleb.about.should.be.empty;
+                done();
+            });
+        });
+    });
+    describe('#remove() celebrity with empty about field', function() {
+        it('should remove celebrity with specified id', function(done) {
+            request.del('http://localhost:3000/celeb/' + newCelebResponse.celebId, {
+                form: {
+                    authToken: privilegedUser.token
+                }
+            }, function(e, r) {
+                r.statusCode.should.equal(200);
+                done();
+            });
+        });
+        it('should not retrieve deleted celebrity', function(done) {
+            request('http://localhost:3000/user/' + newCelebResponse.celebId, function(e, r) {
+                r.statusCode.should.equal(404);
+                done();
+            });
+        });
+    });
+
 });
