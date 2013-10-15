a06236683c042068f1660e0c913ff74bc1b06eba
Alex <aleksandr.kyrychok@acropolium.com>
2013-10-15 15:57:38 +0300

    Allowed same origin policy for experimental period

--- index.js
+++ index.js
@@ -3,6 +3,13 @@ var express = require('express');
 var app = express();
 var mongoose = require('mongoose');
 mongoose.connect('mongodb://localhost/paparazzi');
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', 'http://dev.backbone.localhost');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+}
 
 app.set('views', __dirname + '/views');
 app.use(express.logger('dev'));
@@ -11,6 +18,7 @@ app.use(express.cookieParser('5a87a5b60ff6aabcc06da6ceb22e9ff8c1bfa2ac391e359d07
 app.use(express.session());
 app.use(express.bodyParser());
 app.use(express.methodOverride());
+app.use(allowCrossDomain);
 
 require('./lib/boot')(app, {verbose: true});
 
