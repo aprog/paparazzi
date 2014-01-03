aea3a52cf70cfea3ed3e6efe6ca608d569d4154b
Alex <aleksandr.kyrychok@acropolium.com>
2014-01-03 17:43:06 +0200

    Updated content generation script

--- generate_places.sh
+++ generate_places.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
-PLACES_COUNT=500
-MAX_FILES_PER_RECORD=3
+PLACES_COUNT=30
+MAX_FILES_PER_RECORD=5
+SERVER_URI=10.0.0.239:3000
 
 for (( i=0; i<$PLACES_COUNT; i++ ))
 do
-    random_uid=$(curl -s -X GET localhost:3000/user | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
-    random_celeb=$(curl -s -X GET localhost:3000/celeb | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
-    message="Etiam ultricies nisi vel augue."
+    random_uid=$(curl -s -X GET $SERVER_URI/user | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
+    random_celeb=$(curl -s -X GET $SERVER_URI/celeb | grep '"_id"' | cut -d '"' -f 4 | sort -R | head -n 1)
+    message=$i" Etiam ultricies nisi vel augue."
 
     lat=$(shuf -i 0-90 -n 1).$(shuf -i 0-10000 -n 1)
     lat_sign=$(shuf -i 0-1 -n 1)
@@ -27,9 +28,9 @@ do
     for (( j=0; j<$files_random_count; j++ ))
     do
         if [ -z "$files" ]; then
-            files="-F photos=@"$(ls -pA | grep -v "/$" | sort -R | head -n 1)
+            files="-F photos=@./images/"$(ls ./images -pA | grep -v "/$" | sort -R | head -n 1)
         else
-            files=$files" -F photos=@"$(ls -pA | grep -v "/$" | sort -R | head -n 1)
+            files=$files" -F photos=@./images/"$(ls ./images -pA | grep -v "/$" | sort -R | head -n 1)
         fi
     done
 
@@ -41,10 +42,11 @@ do
         -F userId=$random_uid \
         -F celebId=$random_celeb \
         -F message="$message" \
-        -F loc[latitude]=$lat \
-        -F loc[longtitude]=$long \
+        -F location[latitude]=$lat \
+        -F location[longtitude]=$long \
+        -F authToken=cfe98356e901584a22fe0db32a5dfcb384401adac6db39b2aec0bc61ee5f9a30 \
         $files \
-        localhost:3000/place
+        $SERVER_URI/place
 
     if [ $? -eq 0 ]; then
         printf "\nAdded [%3d] place with location: %9.5f %10.5f\n" $i $lat $long
